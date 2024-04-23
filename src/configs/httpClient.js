01a3c4a8e53c7979f61927916f8b4953df43b759
Vedant Patil <79906441+vedantpatil2021@users.noreply.github.com>
2024-04-23 10:13:54 +0530

    Url Updates

--- src/configs/httpClient.js
+++ src/configs/httpClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:5000/",
+    baseURL: "http://tmsvitflaskapi.eastus.cloudapp.azure.com:5000/",
     withCredentials: true,
 })
